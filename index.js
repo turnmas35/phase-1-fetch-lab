58b4548a0f3ff8bf94cd052a16419732c2f4c06c
Matthew Turner <turnmas35@gmail.com>
2023-11-20 18:00:17 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then((resp) => resp.json())
+  .then((json) => renderBooks(json));
   
+  // .then(res => res.json())
+  // .then(renderBooks => console.log(books))
 }
+
 
 function renderBooks(books) {
   const main = document.querySelector('main');
